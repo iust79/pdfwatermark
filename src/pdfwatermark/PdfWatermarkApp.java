506a911bc62597e349b820e108684f4405034bc1
iust79 <iuli2003raluca@yahoo.com>
2024-04-18 16:56:44 +0300

    last changes

--- src/pdfwatermark/PdfWatermarkApp.java
+++ src/pdfwatermark/PdfWatermarkApp.java
@@ -82,6 +82,61 @@ public class PdfWatermarkApp {
 	}
 
 	
+	public static void modPdfAllPages(String item, int i, String inputPdfPath2, String outputPdfPath2) {
+		
+		
+		PdfDocument pdfDoc;
+		try {
+			pdfDoc = new PdfDocument(new PdfReader(inputPdfPath2+item.toString()), new PdfWriter(outputPdfPath2 + item.toString()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+			
+		ImageData image=null;
+		try {
+			 image = ImageDataFactory.create("i:\\_LOTURI_MF\\T\\TEST\\STAMP\\logo.png");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		        PdfExtGState gs = new PdfExtGState();
+		        gs.setFillOpacity(0.5f); // 50% opacity
+		   
+    	
+        System.out.println("create pdfDoc nr. " +i);
+        // Iterate through each page and add the watermark
+ //       for (int pageNum = 1; pageNum <= pdfDoc.getNumberOfPages(); pageNum++) {
+ //           PdfPage page = pdfDoc.getPage(pageNum);
+        	PdfPage page = pdfDoc.getPage(1);
+        	System.out.println("get page for pdfDoc "+i);
+        	
+            PdfCanvas canvas = new PdfCanvas(page);
+
+            System.out.println("create canvas for pdfDoc "+i);
+            
+            // Position the watermark (e.g., centered)
+   //         Rectangle pageSize = page.getPageSize();
+   //         float x = (pageSize.getLeft() + pageSize.getRight()) / 2;
+   //         float y = (pageSize.getTop() + pageSize.getBottom()) / 2;
+            float x = 50;
+            float y = 10;
+
+
+            // Add the image watermark
+            canvas.saveState();
+            canvas.setExtGState(gs);
+ //           canvas.addImage(watermarkImage, 100, 0, 0, 100, x, y);
+            canvas.addImage(image, 100, 0, 0, 160, y, x);
+            canvas.restoreState();
+  //      }
+            System.out.println("Ready to close pdfDOc nr. "+i);
+            
+        pdfDoc.close();
+	}
+	
 
 	
     public static void main(String[] args) throws Exception {
